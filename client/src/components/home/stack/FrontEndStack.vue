6f6e4442af218c8dd78ca85c9dd250dd63690c1d
fksk94 <fksk94@gmail.com>
2021-09-24 12:54:09 +0900

    refactor: 💡 stack 컴포넌트 분할

--- client/src/components/home/stack/FrontEndStack.vue
+++ client/src/components/home/stack/FrontEndStack.vue
@@ -1,140 +1,53 @@
 <template>
   <v-row>
-    <v-col cols="6" sm="4" md="3" lg="2" class="stackDetail">
-      <img src="@/assets/img/stack/vuejs.jpg" class="stackImg" alt="" />
-      <h4>Vue.js</h4>
-      <v-rating
-        :value="3.5"
-        :readonly="true"
-        color="yellow darken-3"
-        background-color="white"
-        half-increments
-        small
-      ></v-rating>
-    </v-col>
-    <v-col cols="6" sm="4" md="3" lg="2" class="stackDetail">
-      <img src="@/assets/img/stack/vuex.jpg" class="stackImg" alt="" />
-      <h4>Vuex</h4>
-      <v-rating
-        :value="3"
-        :readonly="true"
-        color="yellow darken-3"
-        background-color="white"
-        half-increments
-        small
-      ></v-rating>
-    </v-col>
-    <v-col cols="6" sm="4" md="3" lg="2" class="stackDetail">
-      <img src="@/assets/img/stack/html.jpg" class="stackImg" alt="" />
-      <h4>HTML5</h4>
-      <v-rating
-        :value="3"
-        :readonly="true"
-        color="yellow darken-3"
-        background-color="white"
-        half-increments
-        small
-      ></v-rating>
-    </v-col>
-    <v-col cols="6" sm="4" md="3" lg="2" class="stackDetail">
-      <img src="@/assets/img/stack/css.jpg" class="stackImg" alt="" />
-      <h4>CSS3</h4>
-      <v-rating
-        :value="3.5"
-        :readonly="true"
-        color="yellow darken-3"
-        background-color="white"
-        half-increments
-        small
-      ></v-rating>
-    </v-col>
-    <v-col cols="6" sm="4" md="3" lg="2" class="stackDetail">
-      <img src="@/assets/img/stack/javascript.jpg" class="stackImg" alt="" />
-      <h4>JavaScript</h4>
-      <v-rating
-        :value="3"
-        :readonly="true"
-        color="yellow darken-3"
-        background-color="white"
-        half-increments
-        small
-      ></v-rating>
-    </v-col>
-    <v-col cols="6" sm="4" md="3" lg="2" class="stackDetail">
-      <img src="@/assets/img/stack/typescript.png" class="stackImg" alt="" />
-      <h4>TypeScript</h4>
-      <v-rating
-        :value="1.5"
-        :readonly="true"
-        color="yellow darken-3"
-        background-color="white"
-        half-increments
-        small
-      ></v-rating>
-    </v-col>
-    <v-col cols="6" sm="4" md="3" lg="2" class="stackDetail">
-      <img src="@/assets/img/stack/sass.jpg" class="stackImg" alt="" />
-      <h4>SASS</h4>
-      <v-rating
-        :value="2"
-        :readonly="true"
-        color="yellow darken-3"
-        background-color="white"
-        half-increments
-        small
-      ></v-rating>
-    </v-col>
-    <v-col cols="6" sm="4" md="3" lg="2" class="stackDetail">
-      <img src="@/assets/img/stack/reactjs.jpg" class="stackImg" alt="" />
-      <h4>React.js</h4>
-      <v-rating
-        :value="2"
-        :readonly="true"
-        color="yellow darken-3"
-        background-color="white"
-        half-increments
-        small
-      ></v-rating>
-    </v-col>
-    <v-col cols="6" sm="4" md="3" lg="2" class="stackDetail">
-      <img src="@/assets/img/stack/redux.jpg" class="stackImg" alt="" />
-      <h4>Redux</h4>
-      <v-rating
-        :value="1.5"
-        :readonly="true"
-        color="yellow darken-3"
-        background-color="white"
-        half-increments
-        small
-      ></v-rating>
-    </v-col>
-    <v-col cols="6" sm="4" md="3" lg="2" class="stackDetail">
-      <img src="@/assets/img/stack/reactnative.jpg" class="stackImg" alt="" />
-      <h4>ReacNative</h4>
-      <v-rating
-        :value="1.5"
-        :readonly="true"
-        color="yellow darken-3"
-        background-color="white"
-        half-increments
-        small
-      ></v-rating>
-    </v-col>
+    <StackDetail
+      v-for="(name, idx) in names"
+      :key="idx"
+      :img="imgs[idx]"
+      :name="name"
+      :rating="ratings[idx]"
+    />
   </v-row>
 </template>
 
 <script>
+import StackDetail from "@/components/home/stack/StackDetail.vue";
+
 export default {
   name: "FrontEndStack",
+  data() {
+    return {
+      names: [
+        "Vue.js",
+        "Vuex",
+        "HTML5",
+        "CSS3",
+        "JavaScript",
+        "TypeScript",
+        "SASS",
+        "React.js",
+        "Redux",
+        "ReacNative",
+      ],
+      ratings: [3.5, 3, 3, 3.5, 3, 1.5, 2, 2, 1.5, 1.5],
+      imgs: [
+        "vuejs.jpg",
+        "vuex.jpg",
+        "html.jpg",
+        "css.jpg",
+        "javascript.jpg",
+        "typescript.png",
+        "sass.jpg",
+        "reactjs.jpg",
+        "redux.jpg",
+        "reactnative.jpg",
+      ],
+    };
+  },
+  components: {
+    StackDetail,
+  },
 };
 </script>
 
-<style scoped>
-.stackDetail {
-  text-align: center;
-}
-
-.stackImg {
-  height: 100px;
-}
-</style>
+<style scoped></style>
---
+++ client/src/components/home/stack/StackDetail.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-col cols="6" sm="4" md="3" lg="2" class="stackDetail">
+    <img :src="imgUrl" class="stackImg" alt="" />
+    <h4>{{ name }}</h4>
+    <v-rating
+      :value="rating"
+      :readonly="true"
+      color="yellow darken-3"
+      background-color="white"
+      half-increments
+      small
+    ></v-rating>
+  </v-col>
+</template>
+
+<script>
+export default {
+  name: "StackDetail",
+  props: {
+    img: String,
+    name: String,
+    rating: Number,
+  },
+  computed: {
+    imgUrl () {
+      return require(`../../../assets/img/stack/${this.img}`)
+    }
+  }
+};
+</script>
+
+<style>
+.stackDetail {
+  text-align: center;
+}
+
+.stackImg {
+  height: 100px;
+}
+</style>
